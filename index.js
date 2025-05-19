7efeb5377cff951ab3c13e616e563b192658aef8
Cobray Gordon <epothos@gmail.com>
2025-05-19 16:39:22 +0300

    update

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.static('dist'));
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'));
 
 morgan.token('post', function (req, res) {
     return req.method === 'POST' ? JSON.stringify(req.body) : '';
 });
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'));
 
 let persons = [
     { 
